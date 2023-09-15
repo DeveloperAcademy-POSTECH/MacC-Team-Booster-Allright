ac4660131d78c1a04301c844b112f47114bb4304
ckyeon <qsky8520@gmail.com>
2023-09-16 01:05:49 +0900

    initial commit

---
+++ Allright/AllrightApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllrightApp.swift
+//  Allright
+//
+//  Created by 조기연 on 2023/09/15.
+//
+
+import SwiftUI
+
+@main
+struct AllrightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
