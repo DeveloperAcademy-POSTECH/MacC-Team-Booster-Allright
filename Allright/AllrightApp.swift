0f4680eee007ca6333e7fc221ef96f3eb0c4466a
jinyong <96717313+it794613@users.noreply.github.com>
2023-09-16 16:42:54 +0900

    settings

--- Allright/AllrightApp.swift
+++ Allright/AllrightApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct AllrightApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
---
+++ Allright/Common/Colors.swift
@@ -0,0 +1,28 @@
+//
+//  Colors.swift
+//  Allright
+//
+//  Created by 최진용 on 2023/09/16.
+//
+
+import SwiftUI
+
+struct Colors {
+    static var orange = Color(hex: "FF814A")
+    static var green800 = Color(hex: "5C6334")
+    static var green700 = Color(hex: "858C5B")
+    static var green600 = Color(hex: "9DAD42")
+    static var green500 = Color(hex: "A9BA4A")
+    static var green400 = Color(hex: "BFD449")
+    static var green200 = Color(hex: "E6FF5B")
+    static var green100 = Color(hex: "F0FCAB")
+    static var black = Color(hex: "000000")
+    static var gray900 = Color(hex: "262622")
+    static var gray800 = Color(hex: "484747")
+    static var gray700 = Color(hex: "595A53")
+    static var gray600 = Color(hex: "797B6F")
+    static var gray500 = Color(hex: "8F908A")
+    static var gray300 = Color(hex: "C4C8B6")
+    static var gray100 = Color(hex: "ECEDE8")
+    static var gray50 = Color(hex: "F6F6F0")
+}
