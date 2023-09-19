34338ac78b36cb67fb67fbf2f414558359b8570e
songmoro <97587585+songmoro@users.noreply.github.com>
2023-09-19 16:11:10 +0900

    feat: 레코드 기능 추가

---
+++ Allright/Common/VoiceRecords.swift
@@ -0,0 +1,20 @@
+//
+//  VoiceRecords.swift
+//  Allright
+//
+//  Created by 송재훈 on 2023/09/18.
+//
+
+import SwiftUI
+
+struct VoiceRecords: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VoiceRecords_Previews: PreviewProvider {
+    static var previews: some View {
+        VoiceRecords()
+    }
+}
---
+++ Allright/ViewController/VoiceRecordVM.swift
@@ -0,0 +1,20 @@
+//
+//  VoiceRecordVM.swift
+//  Allright
+//
+//  Created by 송재훈 on 2023/09/18.
+//
+
+import SwiftUI
+
+struct VoiceRecordVM: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VoiceRecordVM_Previews: PreviewProvider {
+    static var previews: some View {
+        VoiceRecordVM()
+    }
+}
